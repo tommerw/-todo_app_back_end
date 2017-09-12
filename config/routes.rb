22a3ddf750448cde50f95d5d14ac5e1218e30f66
Tommer Wizansky <twizansk@gmail.com>
2017-09-12 22:54:21 +0000

    Added show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'todo/index', to: 'todo#index'
+  get 'todo/show', to: 'todo#show'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
