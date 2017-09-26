d0459b680b82c3edc7658659114716ad6e158ece
Tommer Wizansky <twizansk@gmail.com>
2017-09-26 05:01:04 +0000

    Use .each do block on index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'todo/index', to: 'todo#index'
   get 'todo/show/:id', to: 'todo#show'
+  get 'todo/new', to: 'todo#new'
+  get 'todo/create', to: 'todo#create'
+  get 'todo/edit/:id', to: 'todo#edit'
+  get 'todo/update/:id', to: 'todo#update'
+  get 'todo/delete/:id', to: 'todo#delete'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
